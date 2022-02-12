a736c555141ce2dc269dc045deda2c00f0de04ae
MM <tbk40504@outlook.jp>
2022-02-12 21:44:14 +0900

    re rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import { ColoredMessage } from "./components/ColoredMessage";
 import { useState } from "react";
-import { CssModules } from "./components/CssModules";
-// import { useEffect } from "react";
+import { Child1 } from "./components/Child1";
+import { Child4 } from "./components/Child4";
+
 export const App = () => {
+  console.log("Appレンダリング");
+
   const [num, setNum] = useState(0);
 
   const onClickButton = () => {
-    // setNum(num + 1);
-    console.log("レンダリング");
-    setNum((prev) => prev + 1);
+    setNum(num + 1);
   };
 
   return (
     <>
-      <h1 style={{ color: "red" }}>こんんちは!</h1>
-      <ColoredMessage color="blue">お元気ですか</ColoredMessage>
-      <ColoredMessage color="pink">元気です</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
       <p>{num}</p>
-      <CssModules />
+      <Child1 />
+      <Child4 />
     </>
   );
 };
